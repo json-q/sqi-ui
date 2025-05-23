af537ec344ca669b7257a2b231fc6b2ffa0e9362
JSQ <j996730508@163.com>
2025-05-23 09:10:09 +0800

    docs: typo

--- packages/sq-ui/src/space/demos/base.tsx
+++ packages/sq-ui/src/space/demos/base.tsx
@@ -22,7 +22,7 @@ export default function Demo() {
         <Button type="primary">Item2</Button>
         <Button type="primary">Item3</Button>
       </Space>
-      <h5>Custom Size</h5>
+      <h5>Custom gap</h5>
       <Space size={64}>
         <Button type="primary">Item1</Button>
         <Button type="primary">Item2</Button>
