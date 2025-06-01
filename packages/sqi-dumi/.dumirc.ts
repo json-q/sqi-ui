b54944927859b0d833573fb6db3ed29df24a7f56
JSQ <j996730508@163.com>
2025-06-01 17:47:04 +0800

    chore(workflow): fix

--- packages/sqi-dumi/.dumirc.ts
+++ packages/sqi-dumi/.dumirc.ts
@@ -5,6 +5,9 @@ export default defineConfig({
   npmClient: 'pnpm',
   hash: true,
   exportStatic: {},
+  // see https://d.umijs.org/guide/faq#%E9%9D%9E%E6%A0%B9%E7%9B%AE%E5%BD%95%E9%83%A8%E7%BD%B2
+  base: '/sqi-ui/',
+  publicPath: '/sqi-ui/',
   resolve: {
     docDirs: ['docs'],
     atomDirs: [{ type: 'component', dir: '../sqi-web/src' }],
