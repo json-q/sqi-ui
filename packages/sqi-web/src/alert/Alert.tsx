401b5486f20b8ddf1bcff2e088c26cb01fb7c745
jiaosongqi <112090173+json-q@users.noreply.github.com>
2025-07-07 14:09:06 +0800

    feat: alert close support destroy animation (#22)
    * feat: alert close support destroy animation
    
    * style: clean code
    
    * fix

--- packages/sqi-web/src/alert/Alert.tsx
+++ packages/sqi-web/src/alert/Alert.tsx
@@ -1,9 +1,7 @@
 'use client';
-import React, { createElement, forwardRef, isValidElement, useContext, useState } from 'react';
-import { useMergeProps } from '@sqi-ui/hooks';
-import { ConfigContext } from '../config-provider/context';
-import type { AlertProps } from './type';
+import React, { createElement, forwardRef, isValidElement, useContext, useRef } from 'react';
 import clsx from 'clsx';
+import { useMergeProps } from '@sqi-ui/hooks';
 import {
   CheckCircleFilledIcon,
   CloseCircleFilledIcon,
@@ -11,12 +9,16 @@ import {
   InfoCircleFilledIcon,
   CloseIcon,
 } from '@sqi-ui/icons';
+import CSSMotion from '../_common/CSSMotion';
+import { ConfigContext } from '../config-provider/context';
+import type { AlertProps } from './type';
 
 const defaultProps: AlertProps = {
   type: 'info',
+  showIcon: true,
 };
 
-const iconTypeMap = {
+const iconTypeMap: Record<NonNullable<AlertProps['type']>, React.ElementType> = {
   success: CheckCircleFilledIcon,
   info: InfoCircleFilledIcon,
   error: CloseCircleFilledIcon,
@@ -25,52 +27,53 @@ const iconTypeMap = {
 
 const Alert = forwardRef<HTMLDivElement, AlertProps>((baseProps, ref) => {
   const { prefixCls, componentConfig } = useContext(ConfigContext);
-  const {
-    className,
-    style,
-    title,
-    description,
-    type,
-    closable,
-    showIcon = true,
-    action,
-    icon,
-    onClose,
-  } = useMergeProps(baseProps, defaultProps, componentConfig?.Alert);
+  const { className, style, title, description, type, closable, showIcon, action, icon, onClose } = useMergeProps(
+    baseProps,
+    defaultProps,
+    componentConfig?.Alert,
+  );
 
-  const [closed, setClosed] = useState(false);
+  const toggleRef = useRef<(toEnter?: boolean) => void>(null);
 
   const renderIcon = () => {
     if (isValidElement(icon)) return icon;
-    return createElement(iconTypeMap[type!]);
+    if (type) return createElement(iconTypeMap[type]);
+    return null;
   };
 
   const classes = clsx(`${prefixCls}-alert`, `${prefixCls}-alert-${type}`, className);
 
   const handleClose = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    setClosed(true);
+    toggleRef.current?.();
     onClose?.(e);
   };
 
-  if (closed) return null;
-
   return (
-    <div role="alert" className={classes} style={style} ref={ref}>
-      {showIcon && <div className={`${prefixCls}-alert-icon`}>{renderIcon()}</div>}
+    <CSSMotion timeout={200} name="alert" unmountOnExit initialEntered>
+      {({ isMounted, className, toggle }) => {
+        if (!isMounted) return null;
+        toggleRef.current = toggle;
 
-      <div className={`${prefixCls}-alert-content`}>
-        {!!title && <div className={`${prefixCls}-alert-title`}>{title}</div>}
-        <div className={`${prefixCls}-alert-description`}>{description}</div>
-      </div>
+        return (
+          <div role="alert" className={clsx(classes, className)} style={style} ref={ref}>
+            {showIcon && <div className={`${prefixCls}-alert-icon`}>{renderIcon()}</div>}
 
-      {action && <div className={`${prefixCls}-alert-action`}>{action}</div>}
+            <div className={`${prefixCls}-alert-content`}>
+              {!!title && <div className={`${prefixCls}-alert-title`}>{title}</div>}
+              <div className={`${prefixCls}-alert-description`}>{description}</div>
+            </div>
 
-      {closable && (
-        <button className={`${prefixCls}-alert-close`} onClick={handleClose}>
-          <CloseIcon />
-        </button>
-      )}
-    </div>
+            {action && <div className={`${prefixCls}-alert-action`}>{action}</div>}
+
+            {closable && (
+              <button className={`${prefixCls}-alert-close`} onClick={handleClose}>
+                <CloseIcon />
+              </button>
+            )}
+          </div>
+        );
+      }}
+    </CSSMotion>
   );
 });
 
