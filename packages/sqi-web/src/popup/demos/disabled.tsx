e7c6a0f0135e538e2da9fe2e00af9c36988d147d
jiaosongqi <112090173+json-q@users.noreply.github.com>
2025-08-20 22:40:06 +0800

    docs: improve document (#41)

--- packages/sqi-web/src/popup/demos/disabled.tsx
+++ packages/sqi-web/src/popup/demos/disabled.tsx
@@ -6,7 +6,7 @@ export default function Demo() {
   return (
     <Space>
       <Checkbox checked={disabled} onChange={(e) => setDisabled(e.target.checked)}>
-        {disabled ? 'Enable' : 'Disable'}
+        Disable Popup
       </Checkbox>
 
       <Popup disabled={disabled} content="This is a popup">
