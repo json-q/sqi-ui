2fc5e608a5731f0eeaf405fc994325354b9a7d87
JSQ <j996730508@163.com>
2025-08-21 12:54:11 +0800

    chore: clean code

--- packages/sqi-hooks/src/usePrevious.ts
+++ packages/sqi-hooks/src/usePrevious.ts
@@ -1,11 +1,11 @@
 import { useEffect, useRef } from 'react';
 
-export function usePrevious<T>(state: T): React.RefObject<T | undefined> {
+export function usePrevious<T>(state: T): T | undefined {
   const ref = useRef<T>(undefined);
 
   useEffect(() => {
     ref.current = state;
   }, [state]);
 
-  return ref;
+  return ref.current;
 }
