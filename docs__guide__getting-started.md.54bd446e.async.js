4af2249f28de89b0454ebe9bb1177c6f8885e2cf
json-q <json-q@users.noreply.github.com>
2025-10-22 06:41:04 +0000

    deploy: 80847d9a75c94fd91270431c1456d0a2571ab213

---
+++ docs__guide__getting-started.md.54bd446e.async.js
@@ -0,0 +1,44 @@
+"use strict";(self.webpackChunk_sqi_ui_dumi=self.webpackChunk_sqi_ui_dumi||[]).push([[655],{75936:function(t,d,s){s.r(d);var a=s(98954),c=s(1867),x=s(664),h=s(62408),p=s(32449),n=s(5096),i=s(13060),m=s(69371),r=s(14876),l=s(29314),u=s(21077),_=s(47950),e=s(59885);function o(){return(0,e.jsx)(r.dY,{children:(0,e.jsx)(u.Suspense,{fallback:(0,e.jsx)(l.Z,{}),children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h1",{id:"\u5FEB\u901F\u5F00\u59CB",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5FEB\u901F\u5F00\u59CB",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u5FEB\u901F\u5F00\u59CB"]}),(0,e.jsxs)("h2",{id:"\u5B89\u88C5",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5B89\u88C5",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u5B89\u88C5"]}),(0,e.jsx)(n.Z,{lang:"bash",children:_.texts[0].value}),(0,e.jsxs)("h3",{id:"\u57FA\u7840\u4F7F\u7528",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u57FA\u7840\u4F7F\u7528",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u57FA\u7840\u4F7F\u7528"]}),(0,e.jsx)("p",{children:_.texts[1].value}),(0,e.jsx)(n.Z,{lang:"tsx",children:_.texts[2].value}),(0,e.jsxs)("h3",{id:"\u5728-next-\u4E2D\u4F7F\u7528",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5728-next-\u4E2D\u4F7F\u7528",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u5728 Next \u4E2D\u4F7F\u7528"]}),(0,e.jsxs)("p",{children:[_.texts[3].value,(0,e.jsx)("code",{children:_.texts[4].value}),_.texts[5].value,(0,e.jsx)("code",{children:_.texts[6].value})]}),(0,e.jsx)(n.Z,{lang:"ts",highlightLines:[4],children:_.texts[7].value}),(0,e.jsxs)("p",{children:[_.texts[8].value,(0,e.jsx)("code",{children:_.texts[9].value}),_.texts[10].value]}),(0,e.jsx)(n.Z,{lang:"css",children:_.texts[11].value}),(0,e.jsxs)("p",{children:[_.texts[12].value,(0,e.jsx)("code",{children:_.texts[13].value}),_.texts[14].value]}),(0,e.jsx)(n.Z,{lang:"tsx",children:_.texts[15].value}),(0,e.jsxs)("h2",{id:"\u6309\u9700\u52A0\u8F7D",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u6309\u9700\u52A0\u8F7D",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u6309\u9700\u52A0\u8F7D"]}),(0,e.jsxs)("p",{children:[_.texts[16].value,(0,e.jsx)("code",{children:_.texts[17].value}),_.texts[18].value]}),(0,e.jsxs)("h3",{id:"rsbuild-\u4E2D\u6309\u9700\u52A0\u8F7D",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#rsbuild-\u4E2D\u6309\u9700\u52A0\u8F7D",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"rsbuild \u4E2D\u6309\u9700\u52A0\u8F7D"]}),(0,e.jsxs)("p",{children:[_.texts[19].value,(0,e.jsx)("code",{children:_.texts[20].value}),_.texts[21].value,(0,e.jsx)("code",{children:_.texts[22].value}),_.texts[23].value]}),(0,e.jsx)(n.Z,{lang:"ts",children:_.texts[24].value}),(0,e.jsxs)("h3",{id:"\u4F7F\u7528-babel-plugin-import",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u4F7F\u7528-babel-plugin-import",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u4F7F\u7528 babel-plugin-import"]}),(0,e.jsxs)("p",{children:[_.texts[25].value,(0,e.jsx)("a",{href:"https://www.npmjs.com/package/babel-plugin-import",children:_.texts[26].value}),_.texts[27].value]}),(0,e.jsx)(n.Z,{lang:"bash",children:_.texts[28].value}),(0,e.jsx)("p",{children:_.texts[29].value}),(0,e.jsx)(n.Z,{lang:"js",children:_.texts[30].value}),(0,e.jsxs)("h2",{id:"\u517C\u5BB9\u6027",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u517C\u5BB9\u6027",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u517C\u5BB9\u6027"]}),(0,e.jsxs)(i.Z,{children:[(0,e.jsx)("thead",{children:(0,e.jsxs)("tr",{children:[(0,e.jsxs)("th",{children:[(0,e.jsx)("a",{href:"http://godban.github.io/browsers-support-badges/",children:(0,e.jsx)("img",{src:"https://tdesign.gtimg.com/docs/edge_48x48.png",alt:"IE / Edge",width:"24px",height:"24px"})}),(0,e.jsx)("br",{}),_.texts[31].value]}),(0,e.jsxs)("th",{children:[(0,e.jsx)("a",{href:"http://godban.github.io/browsers-support-badges/",children:(0,e.jsx)("img",{src:"https://tdesign.gtimg.com/docs/firefox_48x48.png",alt:"Firefox",width:"24px",height:"24px"})}),(0,e.jsx)("br",{}),_.texts[32].value]}),(0,e.jsxs)("th",{children:[(0,e.jsx)("a",{href:"http://godban.github.io/browsers-support-badges/",children:(0,e.jsx)("img",{src:"https://tdesign.gtimg.com/docs/chrome_48x48.png",alt:"Chrome",width:"24px",height:"24px"})}),(0,e.jsx)("br",{}),_.texts[33].value]}),(0,e.jsxs)("th",{children:[(0,e.jsx)("a",{href:"http://godban.github.io/browsers-support-badges/",children:(0,e.jsx)("img",{src:"https://tdesign.gtimg.com/docs/safari_48x48.png",alt:"Safari",width:"24px",height:"24px"})}),(0,e.jsx)("br",{}),_.texts[34].value]})]})}),(0,e.jsx)("tbody",{children:(0,e.jsxs)("tr",{children:[(0,e.jsx)("td",{children:_.texts[35].value}),(0,e.jsx)("td",{children:_.texts[36].value}),(0,e.jsx)("td",{children:_.texts[37].value}),(0,e.jsx)("td",{children:_.texts[38].value})]})})]})]})})})})}d.default=o},47950:function(t,d,s){s.r(d),s.d(d,{texts:function(){return a}});const a=[{value:`npm i @sqi-ui/web
+`,paraId:0,tocIndex:1},{value:"\u63A8\u8350\u5728 webpack\u3001rspack\u3001vite \u7B49\u73B0\u4EE3\u6784\u5EFA\u5DE5\u5177\u4E2D\u4F7F\u7528\uFF0C\u9700\u5F15\u5165\u5168\u5C40\u7684 css \u53D8\u91CF\u624D\u53EF\u6B63\u5E38\u663E\u793A\u6837\u5F0F\uFF0C\u7EC4\u4EF6\u4F9D\u8D56\u8FD9\u4E9B css \u53D8\u91CF",paraId:1,tocIndex:2},{value:`// \u9700\u5F15\u5165\u5C11\u91CF\u7684\u5168\u5C40\u6837\u5F0F\u5C5E\u6027\u624D\u53EF\u4F7F\u7528
+import '@sqi-ui/web/es/style/index.css';
+import { Button } from '@sqi-ui/web';
+
+export default function App() {
+  return <Button type="primary">Hello World</Button>;
+}
+`,paraId:2,tocIndex:2},{value:"\u9996\u5148\u9700\u8981\u5728 ",paraId:3,tocIndex:3},{value:"next.config.ts",paraId:3,tocIndex:3},{value:" \u4E2D\u914D\u7F6E ",paraId:3,tocIndex:3},{value:"transpilePackages",paraId:3,tocIndex:3},{value:`import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  transpilePackages: ['@sqi-ui/web'],
+};
+
+export default nextConfig;
+`,paraId:4,tocIndex:3},{value:"\u7136\u540E\u5728 ",paraId:5,tocIndex:3},{value:"global.css",paraId:5,tocIndex:3},{value:" \u4E2D\u5F15\u5165\u5168\u91CF\u7684 css \u6837\u5F0F",paraId:5,tocIndex:3},{value:`/* global.css */
+@import '@sqi-ui/web/dist/index.css';
+`,paraId:6,tocIndex:3},{value:"\u4F60\u65E0\u9700\u518D\u6807\u8BB0 ",paraId:7,tocIndex:3},{value:"use client",paraId:7,tocIndex:3},{value:"\uFF0C\u7EC4\u4EF6\u4E2D\u5DF2\u7ECF\u8FDB\u884C\u6807\u8BB0\u6765\u517C\u5BB9 Next.js",paraId:7,tocIndex:3},{value:`import { Button } from '@sqi-ui/web';
+
+export default function Home() {
+  return <Button type="primary">Hello Next</Button>;
+}
+`,paraId:8,tocIndex:3},{value:"\u7EC4\u4EF6\u672C\u8EAB\u9ED8\u8BA4\u4F1A\u88AB\u6784\u5EFA\u5DE5\u5177 tree shake, css \u9700\u4F7F\u7528 ",paraId:9,tocIndex:4},{value:"babel-plugin-import",paraId:9,tocIndex:4},{value:" \u7B49\u5DE5\u5177\u5B9E\u73B0\u6309\u9700\u52A0\u8F7D",paraId:9,tocIndex:4},{value:"\u5982\u679C\u4F7F\u7528 ",paraId:10,tocIndex:5},{value:"rsbuild",paraId:10,tocIndex:5},{value:"\uFF0C\u5219\u5176\u81EA\u5E26\u7C7B\u4F3C\u4E8E ",paraId:10,tocIndex:5},{value:"babel-plugin-import",paraId:10,tocIndex:5},{value:" \u7684\u529F\u80FD",paraId:10,tocIndex:5},{value:`// in rsbuild.config.ts
+  source: {
+    transformImport: [
+      {
+        libraryName: '@sqi-ui/web',
+        libraryDirectory: 'es',
+        style: 'css', // set \`true\` to import .less/.scss
+      },
+    ],
+  },
+`,paraId:11,tocIndex:5},{value:"\u4F60\u53EF\u4EE5\u4F7F\u7528 ",paraId:12,tocIndex:6},{value:"babel-plugin-import",paraId:12,tocIndex:6},{value:" \u6765\u5B9E\u73B0 css \u7684\u6309\u9700\u52A0\u8F7D",paraId:12,tocIndex:6},{value:`npm i babel-plugin-import -D
+`,paraId:13,tocIndex:6},{value:"\u5728 babel \u914D\u7F6E\u4E2D\u52A0\u5165",paraId:14,tocIndex:6},{value:`plugins: [
+  [
+    'babel-plugin-import',
+    {
+      libraryName: '@sqi-ui/web',
+      libraryDirectory: 'es',
+      style: 'css',
+    },
+  ],
+];
+`,paraId:15,tocIndex:6},{value:" IE / Edge",paraId:16,tocIndex:7},{value:"Firefox",paraId:16,tocIndex:7},{value:"Chrome",paraId:16,tocIndex:7},{value:"Safari",paraId:16,tocIndex:7},{value:"Edge >= 84",paraId:16,tocIndex:7},{value:"Firefox >= 83",paraId:16,tocIndex:7},{value:"Chrome >= 84",paraId:16,tocIndex:7},{value:"Safari >= 14.1",paraId:16,tocIndex:7}]}}]);
